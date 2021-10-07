ae62d0a508741bd0aef35e983b466906143ed528
Csaba Schneider <csaba.schneider@gmail.com>
2021-10-07 17:58:57 -0300

    added comments and documentation

--- main.jl
+++ main.jl
@@ -1,3 +1,5 @@
+# this is the main file of the package
+# it has to be included to use the functions
+
 include( "short_question.jl" )
 include( "questionnaire.jl" )
-
--- short_question.jl
+++ short_question.jl
@@ -1,19 +1,33 @@
+# this file contains the functions related to questions
+# of type shortanswer
+
+@doc """
+The data structure to hold a short answer type Moodle question
+""" ->
+
 struct short_answer_question
-    title::String
-    text::String
-    answer::String
-    penalty::Float64
-    tags::Vector{String}
-    defgrade::Int64
+    title::String               # the title of the question
+    text::String                # the text of the question
+    answer::String              # the right answer
+    penalty::Float64            # required by Moodle, it's usually 1.0
+    tags::Vector{String}        # list of tags
+    defgrade::Int64             # required by Moodle, usuall 1
 end 
+
+# the following function creates short_answer_question from the first 
+# three attributes -- the others will take the default value
 
 function short_answer_question( title, text, answer )
     
     return short_answer_question( title, text, answer, 0.1, [], 1 )
 end 
 
+@doc """
+converts this type of function to XML string
+""" ->
+
 function QuestionToXML( question::short_answer_question )
-    
+   
     xmlstring = "<question type=\"shortanswer\">\n<name format=\"html\">\n"*
             "<text><![CDATA["*question.title*"]]></text>\n"*
             "</name>\n"*
@@ -28,6 +42,7 @@ function QuestionToXML( question::short_answer_question )
             "<answer fraction=\"100\" format=\"plain_text\"><text>"*
             question.answer*"</text>\n</answer>\n"
 
+    # if question has at least one tag, then they are added
     if length( question.tags ) > 0  
         xmlstring *= "<tags>\n"
         for tag in question.tags 
@@ -36,6 +51,7 @@ function QuestionToXML( question::short_answer_question )
         xmlstringtext *= "</tags>\n"
     end
 
+    # end of question
     xmlstring *= "</question>\n" 
     return xmlstring
 end
