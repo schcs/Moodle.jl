68d2722fd71ca9853fb1469632aec46fa16742b2
Csaba Schneider <csaba.schneider@gmail.com>
2022-03-09 10:27:44 -0300

    put deploy in make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -13,8 +13,8 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=
+
 deploydocs(
     repo = "https://schcs.github.io/Moodle.jl/"
 )
-=#
+
